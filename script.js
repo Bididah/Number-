d2855c700c687dfca30a29c9e0f509beb8d21d99
ENEMIE <hanfihamza20@.gmail.com>
2021-01-02 03:05:54 +0100

    Number Guesser finished but still need some samll changes

--- script.js
+++ script.js
@@ -3,4 +3,25 @@ let computerScore = 0;
 let currentRoundNumber = 1;
 
 // Write your code below:
+const generateTarget = () => {
+    return Math.floor(Math.random()* 9)
+}
+const compareGuesses = (human , computer , secretTarget) => {
+    if (Math.abs(secretTarget-human) <= Math.abs(secretTarget-computer)) {
+        return true ;
+    } else {
+        return false ; 
+    }
 
+}
+const updateScore = (string) => { 
+    if (string === 'human') {
+        humanScore ++ ;
+        
+    }else if (string === 'computer') {
+        computerScore ++ ;
+    }
+}
+const advanceRound = () => {
+    currentRoundNumber ++ ; 
+}
