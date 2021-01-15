2891c1e79412086d3d8b744b1da029049252ea97
ENEMIE <hanfihamza20@.gmail.com>
2021-01-15 15:19:23 +0100

    work on Mini Linter

--- projects.js
+++ projects.js
@@ -267,11 +267,26 @@ const newSum1 = newNumbers.reduce((accumulator, currentValue ) => {
  // Make sure to uncomment the code below and fix the incorrect code before running it
  
  // console.log(interestingWords.every((word) => { } ));
-let story = 'Last weekend, I took literally the most beautiful bike ride of my life. The route is called "The 9W to Nyack" and it actually stretches all the way from Riverside Park in Manhattan to South Nyack, New Jersey. It\'s really an adventure from beginning to end! It is a 48 mile loop and it basically took me an entire day. I stopped at Riverbank State Park to take some extremely artsy photos. It was a short stop, though, because I had a really long way left to go. After a quick photo op at the very popular Little Red Lighthouse, I began my trek across the George Washington Bridge into New Jersey.  The GW is actually very long - 4,760 feet! I was already very tired by the time I got to the other side.  An hour later, I reached Greenbrook Nature Sanctuary, an extremely beautiful park along the coast of the Hudson.  Something that was very surprising to me was that near the end of the route you actually cross back into New York! At this point, you are very close to the end.';
+let story = 'hamza Last weekend, I took literally the most beautiful bike ride of my life. The route is called "The 9W to Nyack" and it actually stretches all the way from Riverside Park in Manhattan to South Nyack, New Jersey. It\'s really an adventure from beginning to end! It is a 48 mile loop and it basically took me an entire day. I stopped at Riverbank State Park to take some extremely artsy photos. It was a short stop, though, because I had a really long way left to go. After a quick photo op at the very popular Little Red Lighthouse, I began my trek across the George Washington Bridge into New Jersey.  The GW is actually very long - 4,760 feet! I was already very tired by the time I got to the other side.  An hour later, I reached Greenbrook Nature Sanctuary, an extremely beautiful park along the coast of the Hudson.  Something that was very surprising to me was that near the end of the route you actually cross back into New York! At this point, you are very close to the end.';
 const storywords = story.split(' ')
 console.log(storywords)
 console.log(storywords.length)
 
 let overusedWords = ['really', 'very', 'basically'];
 
-let unnecessaryWords = ['extremely', 'literally', 'actually' ];
+let unnecessaryWords = ['extremely', 'literally', 'actually',];
+betterWords = storywords.filter(element => {
+ return  unnecessaryWords.includes(element) === false })
+console.log(betterWords)
+const NUMBERoFoverusedWord = (array) => {
+   let n = 0
+   for(i=0; i < overusedWords.length ; i++ ) {
+          array.forEach(element => {
+             if(element === overusedWords[i]){
+                n ++ 
+             }
+          })
+          }
+   return `the number of overusedword combined is ${n}`
+   }
+console.log(NUMBERoFoverusedWord(betterWords))
